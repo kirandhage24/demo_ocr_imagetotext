a781447f371024ad0a7378a015abeea23f7cef37
unknown <kirankumar.ada@gmail.com>
2026-03-29 17:23:31 +0530

    uploading code to git

---
+++ image_text.py
@@ -0,0 +1,36 @@
+from groq import Groq
+import base64
+import os
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = "D:/Gradus/test_bill.jpeg"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+client = Groq(api_key="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")
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What's in this image?"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                },
+            ],
+        }
+    ],
+    model="meta-llama/llama-4-scout-17b-16e-instruct",
+)
+
+print(chat_completion.choices[0].message.content)
